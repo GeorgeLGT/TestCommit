3e5dd044e47ffc2ee165ff7248b8319f91be1e02
Georgy Lesyuk <rambozet@yandex.ru>
2025-06-17 20:51:40 +0300

    Коммит с изменениями - 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Коммит без изменений");
+        System.out.println("Коммит с изменениями номер 1");
     }
 }
